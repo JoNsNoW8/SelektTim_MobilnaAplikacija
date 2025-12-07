52ae486e96b804731f20c879072830524ed1a67c
JoNsNoW8 <nmojsijevic4@gmail.com>
2025-12-07 13:48:59 +0100

    inicijalni navbar

---
+++ lib/navbar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobilne/screens/bar_code_screen.dart';
+import 'package:mobilne/screens/home_screen.dart';
+import 'package:mobilne/screens/search_screen.dart';
+
+class Navbar extends StatefulWidget{
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar>{
+  late List<Widget> screens;
+  int currentScreen = 0;
+  late PageController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    screens = const [
+      HomeScreen(),
+      SearchScreen(),
+      BarCodeScreen()
+    ];
+
+    controller = PageController(initialPage: currentScreen);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        physics: const NeverScrollableScrollPhysics(),
+        children: screens,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentScreen,
+        height: kBottomNavigationBarHeight,
+        onDestinationSelected: (index){ setState(() {
+          currentScreen = index;
+          controller.jumpToPage(currentScreen);
+        });},
+        destinations: const[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan',
+          ),
+        ],
+      ),
+    );
+  }
+}
